9c0218c197869ff28660468ee8ff766c1b4844b6
Vladislav Knyshov <inova136@gmail.com>
2023-07-22 22:12:20 +0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: process.env.ORIGIN,
+    credentials: true,
   });
   await app.listen(3000);
 }
